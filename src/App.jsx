9503777802134a14133bf75c6092edc1271fcf2a
Elle <ellefamkar@gmail.com>
2025-03-17 12:26:32 +1100

    feat : Add custom render using test-utils for future development sample

--- src/App.jsx
+++ src/App.jsx
@@ -1,48 +1,19 @@
 import { useState } from "react";
 import "./App.css";
 import NoteHeader from "./components/NoteHeader";
-import { NotesProvider } from "./context/NotesContext";
 import NoteApp from "./components/NoteApp";
+import AppProviders from "./providers/AppProviders";
 
 function App() {
-  // let [notes, setNotes] = useState([]);
   let [sortBy, setSortBy] = useState("latest");
 
-  // const handleAddNote = (newNote) => {
-  //   dispatch({ type: "Add", payload: newNote });
-  //   // setNotes((prevNotes) => [...prevNotes, newNote]);
-  // };
-
-  // const handleDeleteNote = (id) => {
-  //   //Approach one
-  //   // const filteredNote = notes.filter((note) => note.id !== id);
-  //   // setNotes(filteredNote);
-
-  //   //Approach Two
-  //   // setNotes((prevNotes) => prevNotes.filter((note) => note.id !== id));
-  //   dispatch({ type: "Delete", payload: id });
-  // };
-
-  // const handleCompleteNote = (e) => {
-  //   const noteId = Number(e.target.value);
-  //   // const newNotes = notes.map((note) => note.id === noteId ? {...note, completed: !note.completed} :  note)
-  //   // setNotes(newNotes);
-
-  //   // setNotes((prevNotes) =>
-  //   //   prevNotes.map((note) =>
-  //   //     note.id === noteId ? { ...note, completed: !note.completed } : note
-  //   //   )
-  //   // );
-  //   dispatch({ type: "Completed", payload: noteId });
-  // };
-
   return (
-    <NotesProvider>
+    <AppProviders>
       <div className="container">
         <NoteHeader sortBy={sortBy} onSort={(e) => setSortBy(e.target.value)} />
         <NoteApp sortBy={sortBy} />
       </div>
-    </NotesProvider>
+    </AppProviders>
   );
 }
 
--- src/components/__test__/NoteApp.test.jsx
+++ src/components/__test__/NoteApp.test.jsx
@@ -1,12 +1,14 @@
-import { fireEvent, render, screen } from "@testing-library/react";
+// import { fireEvent, render, screen } from "@testing-library/react";
+import { fireEvent, render, screen } from "../../test-utils";
 import { expect, test } from "vitest";
 import NoteApp from "../NoteApp";
-import { NotesProvider } from "../../context/NotesContext";
 
+// function baraye inke modam code ro nanevisim ---
 function addNote(notes) {
   const inputTitle = screen.getByPlaceholderText(/New Note/i);
   const inputDescription = screen.getByPlaceholderText(/Note description.../i);
   const button = screen.getByRole("button", { name: /Add New Note/i });
+
   notes.forEach((note) => {
     fireEvent.change(inputTitle, { target: { value: note.title } });
     fireEvent.change(inputDescription, { target: { value: note.description } });
@@ -14,39 +16,54 @@ function addNote(notes) {
   });
 }
 
+//Check "input" is "empty" after submit ------
 test("Note App #1: input should be  empty after submit ", () => {
-  render(<NoteApp sortBy="latest" />, { wrapper: NotesProvider });
+  render(<NoteApp sortBy="latest" />);
   addNote([{ title: "Note one title", description: "Note one description" }]);
+
   const inputTitle = screen.getByPlaceholderText(/New Note/i);
   const inputDescription = screen.getByPlaceholderText(/Note description.../i);
+
   expect(inputTitle.value).toBe("");
   expect(inputDescription.value).toBe("");
 });
 
+
+// Check it adds multiple Notes - integration test ----------
 test("Note App #2: Should add multiple notes ", () => {
-  render(<NoteApp sortBy="latest" />, { wrapper: NotesProvider });
+  render(<NoteApp sortBy="latest" />);
   addNote([
     { title: "Note one title", description: "Note one description" },
     { title: "Note one title", description: "Note one description" },
     { title: "Note one title", description: "Note one description" },
     { title: "Note one title", description: "Note one description" },
   ]);
+
+  //element e sakhte shode ro check mikonim 
   const divElement = screen.getAllByTestId("note-item");
   expect(divElement.length).toBe(4);
 });
 
+
+// Should not have completed class in first initialization --------
 test("Note App #3: Should not have active class in initial render ", () => {
-  render(<NoteApp sortBy="latest" />, { wrapper: NotesProvider });
+  render(<NoteApp sortBy="latest" />);
   addNote([{ title: "Note one title", description: "Note one description" }]);
-  const divElement = screen.getByTestId("note-item");
-  expect(divElement).not.toHaveClass("completed");
+
+  const initialDiv = screen.getByTestId("note-item");
+  expect(initialDiv).not.toHaveClass("completed");
 });
 
+
+
+// Should have complete class when clicked on checkbox
 test("Note App #4: Should have complete class in when clicked on complete", () => {
-  render(<NoteApp sortBy="latest" />, { wrapper: NotesProvider });
+  render(<NoteApp sortBy="latest" />);
   addNote([{ title: "Note one title", description: "Note one description" }]);
+
   const checked = screen.getByRole("checkbox");
   fireEvent.click(checked);
+
   const divElement = screen.getByTestId("note-item");
   expect(divElement).toHaveClass("completed");
 });
